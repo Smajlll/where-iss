0e2dae4b53bc7fbf59ca49eab5d74977b8e62273
Vít Smolík <vit.smolik2@gmail.com>
2024-06-27 16:30:28 +0200

    map is working now, the google maps link is also working, the only thing missing is the design + added loading page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 import './App.css';
+import icon from './satellite.png'
+import 'leaflet/dist/leaflet.css';
 
 function App() {
 
   // api https://api.wheretheiss.at/v1/satellites/25544
   // get: latitude, longitude, altitude, visibility, timestamp (unix time)
 
+  let customIcon = L.icon({
+    iconUrl: icon,
+    iconSize: [50, 50],
+    iconAnchor: [25, 25],
+    popupAnchor: [0, -25]
+  });
+  
   let url = 'https://api.wheretheiss.at/v1/satellites/25544';
 
   let [latitude, setLatitude] = useState(0);
@@ -13,28 +24,43 @@ function App() {
   let [altitude, setAltitude] = useState(0);
   let [visibility, setVisibility] = useState('idk bro');
   let [time, setTime] = useState(0);
+  let [isLoading, setIsLoading] = useState(true);
   let isVisible;
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      setLatitude(data.latitude);
-      setLongitude(data.longitude);
-      setAltitude(data.altitude);
-      setVisibility(data.visibility);
-      setTime(data.timestamp);
-    })
-    .catch(error => console.error('Error fetching quote:', error));
+  useEffect(() => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setLatitude(data.latitude);
+        setLongitude(data.longitude);
+        setAltitude(data.altitude);
+        setVisibility(data.visibility);
+        setTime(data.timestamp);
+        setIsLoading(false);
+      })
+      .catch(error => console.error('error:', error));
+  }, []);
 
+  if (isLoading) {
+    return <div className='loading'>
+      <h1>Loading...</h1>
+      <p>If this takes too long, refresh the page, or check the logs and contact me at me@smoliicek.xyz</p>
+      </div>;
+  }
+  
+    let mapsURL = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
+    
     if (visibility === 'daylight') {
-      isVisible = "The ISS is in daylight! :D";
+      isVisible = "The ISS is in daylight! ☀️";
     } else {
       isVisible = "The ISS is in the Earths shadow. :(";
     }
-    
+
+
 
 
   return (
+    
   <div>    
     <div className="name">
       <h1>Where is the ISS?</h1>
@@ -50,17 +76,29 @@ function App() {
       <p>Altitude: {altitude} km</p>
       <p>{isVisible}</p>
     </div>
+    
+    <div className='map'>      
+      <MapContainer center={[latitude, longitude]} zoom={12}scrollWheelZoom={true}>
+      <TileLayer
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
 
-    <div className='map'>
-      {/* https://developers.google.com/maps/documentation/embed/embedding-map */}
-      <p>THE MAP (the location of ISS on gmaps) WILL BE HERE</p>
-      <p>link to google maps: figuring out, wait, thx</p>
+      <Marker position={[latitude, longitude]} icon={customIcon} open={true}>
+        <Popup>
+          The ISS is up in the sky somewhere around here.
+        </Popup>
+      </Marker>
+    </MapContainer>
+
+
+    <p>Link to Google Maps: <a href={mapsURL} target="_blank" rel="noopener noreferrer">View on Google Maps</a></p>
     </div>
 
     </div>
 
     <div className='bottom'>
-      <button onClick={() => window.location.reload(false)}>Get new data</button>
+    <button onClick={() => window.location.reload(false)}>Where is it now? I want to know!</button>
     </div>
   </div>
   );
